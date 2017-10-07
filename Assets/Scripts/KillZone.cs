459e7ced23ef848da28c44a23f0e27f153fbc852
elninowang <elninowang@qq.com>
2017-10-07 23:07:22 +0800

    basicly complete the project

---
+++ Assets/Scripts/KillZone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour {
+
+    public float Damage = 100f;
+    
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+        if (PlayerControl.PlayerInstance != null)
+            PlayerControl.Health -= Damage * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/QuestGiver.cs
@@ -0,0 +1,36 @@
+﻿//--------------------------------
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+//--------------------------------
+public class QuestGiver : MonoBehaviour
+{
+	//--------------------------------
+	//Human readable quest name
+	public string QuestName = string.Empty;
+	//Reference to UI Text Box
+	public Text Captions = null;
+	//List of strings to say
+	public string[] CaptionText;
+	//--------------------------------
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+        Debug.Log("QuestGiver::OnTriggerEnter2D");
+        if (!other.CompareTag("Player"))return;
+
+		Quest.QUESTSTATUS Status = QuestManager.GetQuestStatus(QuestName);
+		Captions.text = CaptionText[(int) Status];
+	}
+	//--------------------------------
+	void OnTriggerExit2D(Collider2D other) 
+	{
+        Debug.Log("QuestGiver::OnTriggerExit2D");
+        Quest.QUESTSTATUS Status = QuestManager.GetQuestStatus(QuestName);
+		if(Status == Quest.QUESTSTATUS.UNASSIGNED)
+			QuestManager.SetQuestStatus(QuestName, Quest.QUESTSTATUS.ASSIGNED);
+
+		if(Status == Quest.QUESTSTATUS.COMPLETE)
+			Application.LoadLevel(5);
+	}
+}
+//--------------------------------
